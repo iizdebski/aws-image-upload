c54aa2cd06c7d9bd363fb2f10078d142d3ff454f
iizdebski <39988554+iizdebski@users.noreply.github.com>
2020-08-11 16:24:25 +0300

    14_rendering_user_profile

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,9 +5,12 @@ import axios from "axios";
 
 const UserProfiles = () => {
 
+  const [userProfiles, setUserProfiles] = useState([]);
+
   const fetchUserProfiles = () => {
     axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
-      console.log(res);
+      console.log(res);     
+      setUserProfiles(res.data);
     });
   }
 
@@ -15,8 +18,15 @@ const UserProfiles = () => {
     fetchUserProfiles();
   }, []);
 
-  return <h1>Hello</h1>
-}
+  return userProfiles.map((userProfile, index) => {
+    return ( 
+    <div key={index}>
+      <h1>{userProfile.username}</h1>
+    <p>{userProfile.userProfileId}</p>
+    </div>
+    )
+  })
+};
 
 function App() {
   return <div className="App">
