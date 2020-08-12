42e556eba3bc8217c3c58d14bdc65fa1f6e7fbc2
iizdebski <39988554+iizdebski@users.noreply.github.com>
2020-08-12 17:01:50 +0300

    26_unnecessary_data_deletion

--- src/main/java/com/izdebski/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/izdebski/awsimageupload/config/AmazonConfig.java
@@ -14,8 +14,7 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIAICQP3P7FZKFV7A5A",
-                "kMFKj3Oc04SuCC9pW01QYiU2hE3kHo35uqdLMCtM"
+
     );
 
         return AmazonS3ClientBuilder
